4b07a269946a27148ad69df3b5bb565e0899b9a3
이희수 <2heedol@naver.com>
2023-03-22 17:03:52 +0900

    feat: OAuth2 인증 구현

--- backend/src/main/java/com/gog/starrynight/config/SecurityConfig.java
+++ backend/src/main/java/com/gog/starrynight/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.gog.starrynight.config;
 
+import com.gog.starrynight.security.oauth.PrincipalOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PrincipalOAuth2UserService principalOauth2UserService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests() // 애플리케이션에 들어오는 요청에 대한 사용 권한을 체크
@@ -22,6 +25,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()           // CSRF off
                 .httpBasic().disable()     // Basic Auth off
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);    // Session off
+
+        http.oauth2Login() // OAuth2 로그인 설정
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService); // OAuth2인증 과정에서 Authentication 생성에 필요한 OAuth2User를 반환하는 클래스 지정
 
         http.logout()
                 .disable();
