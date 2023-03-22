6eea57928b6115b46fa9bf30aec5934255db66d9
leejinsung <jsmon3536@gmail.com>
2023-03-22 17:43:57 +0900

    Feat: Router 추가.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Main from './Presentational/Page/Main';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </div>
   );
 }
