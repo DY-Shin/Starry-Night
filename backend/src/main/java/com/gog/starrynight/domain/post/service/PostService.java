35fb3b245ead897c50eb171395351b6fe9cfad56
이희수 <2heedol@naver.com>
2023-03-23 14:07:10 +0900

    feat: 단순 게시물 생성 구현

--- backend/src/main/java/com/gog/starrynight/domain/post/service/PostService.java
+++ backend/src/main/java/com/gog/starrynight/domain/post/service/PostService.java
@@ -1,6 +1,12 @@
 package com.gog.starrynight.domain.post.service;
 
+import com.gog.starrynight.common.exception.ResourceNotFoundException;
+import com.gog.starrynight.domain.post.dto.PostInfo;
+import com.gog.starrynight.domain.post.dto.PostRequest;
+import com.gog.starrynight.domain.post.entity.Post;
 import com.gog.starrynight.domain.post.repository.PostRepository;
+import com.gog.starrynight.domain.user.entity.User;
+import com.gog.starrynight.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +16,24 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public PostInfo createPost(PostRequest dto, Long requesterId) {
+        User requester = userRepository.findById(requesterId).orElseThrow(
+                () -> new ResourceNotFoundException("존재하지 않는 회원입니다.")
+        );
+
+        Post post = Post.builder()
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .lat(dto.getLat())
+                .lng(dto.getLng())
+                .writer(requester)
+                .build();
+
+        postRepository.save(post);
+
+        return new PostInfo(post);
+    }
 }
