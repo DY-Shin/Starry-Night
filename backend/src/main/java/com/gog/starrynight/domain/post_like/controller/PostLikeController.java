179ff169e511771ca5715df6974f2dcc4348ddd1
kavin816@naver.com <kavin816@naver.com>
2023-03-23 15:32:12 +0900

    feat: 게시물 좋아요 구현

--- backend/src/main/java/com/gog/starrynight/domain/post_like/controller/PostLikeController.java
+++ backend/src/main/java/com/gog/starrynight/domain/post_like/controller/PostLikeController.java
@@ -1,13 +1,26 @@
 package com.gog.starrynight.domain.post_like.controller;
 
+import com.gog.starrynight.common.dto.ApiResponse;
 import com.gog.starrynight.domain.post_like.service.PostLikeService;
+import com.gog.starrynight.security.LoginUser;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "게시물 좋아요 관리")
 @RestController
 @RequiredArgsConstructor
 public class PostLikeController {
     private final PostLikeService postLikeService;
+
+    @PostMapping("/posts/{postId}/like")
+    public ResponseEntity<ApiResponse> createPostLike(@AuthenticationPrincipal LoginUser loginUser,
+                                                        @PathVariable("postId") Long postId) {
+        postLikeService.createPostLike(postId, loginUser.getId());
+        ApiResponse result = new ApiResponse(true, "좋아요 성공");
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
 }
