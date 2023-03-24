b754dc94c79d00f903592ffeb1f122decb34c42d
이희수 <2heedol@naver.com>
2023-03-24 09:38:53 +0900

    feat: 회원 이름 변경 기능 구현

--- backend/src/main/java/com/gog/starrynight/domain/user/service/UserService.java
+++ backend/src/main/java/com/gog/starrynight/domain/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.gog.starrynight.domain.user.service;
 
+import com.gog.starrynight.common.exception.ResourceNotFoundException;
+import com.gog.starrynight.domain.user.dto.UserNameUpdateRequest;
+import com.gog.starrynight.domain.user.entity.User;
 import com.gog.starrynight.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional
+    public void updateUserName(Long requesterId, UserNameUpdateRequest dto) {
+        User requester = userRepository.findById(requesterId)
+                .orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 회원입니다."));
+
+        requester.setName(dto.getName());
+    }
 }
