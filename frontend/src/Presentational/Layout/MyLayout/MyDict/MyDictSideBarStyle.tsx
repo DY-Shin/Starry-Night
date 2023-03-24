f48b68d44b028cdb1bb92ebef29c5d6c9c1c0208
Lee_Jungyun <leejy96kr@naver.com>
2023-03-24 10:22:43 +0900

    feat: MyDictPageSide_Main Init

---
+++ frontend/src/Presentational/Layout/MyLayout/MyDict/MyDictSideBarStyle.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: fixed;
+  height: 50vh;
+  color: #f0f0f0;
+  background-color: darkorange;
+  opacity: 100%;
+  width: 8vh;
+`;
+
+export const DictNav = styled.div`
+  display: list-item;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+export const DictItem = styled.div`
+  margin: 0.5vh;
+  height: 5vh;
+  font-size: 100%;
+  background-color: navy;
+  border-radius: 1vh;
+`;
--- frontend/src/Presentational/Page/MyPage/MyDict.tsx
+++ frontend/src/Presentational/Page/MyPage/MyDict.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import * as MyStyle from '../../Components/MyComponents/MyStyle';
+import MyDictSideBar from '../../Layout/MyLayout/MyDict/MyDictSideBar';
+import MyDictMainBox from '../../Layout/MyLayout/MyDict/MyDictMainBox';
 
 function MyDict() {
   return (
     <MyStyle.Container>
       <div className="MyDict">
         <h1>MyDict</h1>
+        <MyDictSideBar />
+        <MyDictMainBox />
       </div>
     </MyStyle.Container>
   );
