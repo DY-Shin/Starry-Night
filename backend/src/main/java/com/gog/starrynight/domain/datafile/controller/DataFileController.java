bd6fa9c7986e7e1e150d9d68b5fb9dbc2eb0b7d4
이희수 <2heedol@naver.com>
2023-03-24 13:30:24 +0900

    feat: 데이터 파일 조회 기능 구현

--- backend/src/main/java/com/gog/starrynight/domain/datafile/controller/DataFileController.java
+++ backend/src/main/java/com/gog/starrynight/domain/datafile/controller/DataFileController.java
@@ -2,18 +2,21 @@ package com.gog.starrynight.domain.datafile.controller;
 
 import com.gog.starrynight.common.dto.ApiResponse;
 import com.gog.starrynight.domain.datafile.dto.DataFileInfo;
+import com.gog.starrynight.domain.datafile.dto.DataFileResource;
 import com.gog.starrynight.domain.datafile.service.DataFileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 @Tag(name = "파일 관리")
 @RestController
@@ -27,5 +30,18 @@ public class DataFileController {
         DataFileInfo dataFileInfo = dataFileService.saveDataFile(multipartFile);
         ApiResponse<DataFileInfo> result = new ApiResponse<>(true, "파일 등록 성공", dataFileInfo);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "파일 조회")
+    @GetMapping("/datafiles/{dataFileId}")
+    public ResponseEntity<Resource> getDataFile(@PathVariable Long dataFileId) throws MalformedURLException {
+        DataFileResource dataFileResource = dataFileService.getDataFileResource(dataFileId);
+
+        String contentDisposition = "inline; filename=\"" + dataFileResource.getEncodedFileName() + "\"";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(dataFileResource.getContentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(dataFileResource);
     }
 }
