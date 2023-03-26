b99a9f1c6fb7d1a37e360b1e88b0151bb8740379
Lee_Jungyun <leejy96kr@naver.com>
2023-03-27 02:00:33 +0900

    feat: MyBadge

---
+++ frontend/src/Presentational/Components/MyBadge.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const RewardContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 50%;
+  width: 100%;
+`;
+export const Badge = styled.div`
+  width: 250px;
+  height: 250px;
+  display: flex;
+  font-size: 2rem;
+  color: whitesmoke;
+  align-items: center;
+  justify-content: center;
+  background: #016040;
+  border-radius: 50%;
+  border-color: pink;
+  border-width: 3px;
+  border-style: solid;
+`;
--- frontend/src/Presentational/Page/MyPage/MyReward.tsx
+++ frontend/src/Presentational/Page/MyPage/MyReward.tsx
@@ -2,17 +2,43 @@ import React from 'react';
 import * as MyStyle from '../../Components/MyComponents/MyStyle';
 import MyGlobal from '../../Components/MyComponents/MyGlobalStyle';
 import MyHeader from '../../Layout/MyLayout/MyHeader';
+import * as MyBadge from '../../Components/MyBadge';
 
-function MyBadge() {
+function MyReward() {
   return (
     <MyStyle.Container>
       <MyGlobal />
       <MyHeader />
-      <div className="MyBadge">
-        <h1>MyReward</h1>
-      </div>
+      <MyBadge.RewardContainer>
+        <MyBadge.Badge>
+          곰 두마리가 <br /> 북쪽에 있어
+        </MyBadge.Badge>
+        <MyBadge.Badge>
+          낮에는 꽃잎 <br /> 밤에는 별빛
+        </MyBadge.Badge>
+        <MyBadge.Badge>
+          은하에 흐르는 <br /> 한여름밤의 꿈
+        </MyBadge.Badge>
+        <MyBadge.Badge>
+          낙엽에 적혀진 <br /> 영웅의 이야기
+        </MyBadge.Badge>
+      </MyBadge.RewardContainer>
+      <MyBadge.RewardContainer>
+        <MyBadge.Badge>
+          땅에선 캐롤이 <br /> 하늘에선 별이
+        </MyBadge.Badge>
+        <MyBadge.Badge>
+          멀지만 가까운 <br /> 태양계 가족
+        </MyBadge.Badge>
+        <MyBadge.Badge>
+          천개의 별도 <br /> 첫 관측부터
+        </MyBadge.Badge>
+        <MyBadge.Badge>
+          눈으로 볼 수 <br /> 있는건 다 봤어
+        </MyBadge.Badge>
+      </MyBadge.RewardContainer>
     </MyStyle.Container>
   );
 }
 
-export default MyBadge;
+export default MyReward;
