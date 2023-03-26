4f210e37553b5a2b3caaa1724fb7f5eee914418e
Lee_Jungyun <leejy96kr@naver.com>
2023-03-27 01:03:33 +0900

    feat: MyPost_SearchBar

--- frontend/src/Presentational/Page/MyPage/MyPost.tsx
+++ frontend/src/Presentational/Page/MyPage/MyPost.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import * as MyStyle from '../../Components/MyComponents/MyStyle';
 import MyGlobal from '../../Components/MyComponents/MyGlobalStyle';
 import MyHeader from '../../Layout/MyLayout/MyHeader';
+import MySearchBox from '../../Layout/MyLayout/MyPost/MySearchBox';
 
 function MyPost() {
   return (
     <MyStyle.Container>
       <MyGlobal />
       <MyHeader />
+      <MySearchBox />
       <div className="MyPost">
         <h1>MyPost</h1>
       </div>
