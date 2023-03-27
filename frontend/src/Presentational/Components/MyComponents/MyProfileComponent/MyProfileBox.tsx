f33e4a792e869b8573dc6638769880035883ba5a
Lee_Jungyun <leejy96kr@naver.com>
2023-03-27 17:13:15 +0900

    feat: MyProfile Component added

---
+++ frontend/src/Presentational/Components/MyComponents/MyProfileComponent/MyProfileBox.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const ProfileMainContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: black;
+  height: 90%;
+  width: 100%;
+`;
+
+export const ProfileSubContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: white;
+  height: 90%;
+  width: 50%;
+`;
+
+export const ProfileButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: red;
+  height: 90%;
+  width: 50%;
+`;
+
+export const Photo = styled.div`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  font-size: 1.5rem;
+  color: whitesmoke;
+  align-items: center;
+  justify-content: center;
+  background: #016040;
+  border-radius: 50%;
+  border-color: pink;
+  border-width: 3px;
+  border-style: solid;
+`;
+
+export const Nick = styled.div`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  font-size: 1.5rem;
+  color: whitesmoke;
+  align-items: center;
+  justify-content: center;
+  background: #016040;
+  border-radius: 10%;
+  border-color: purple;
+  border-width: 3px;
+  border-style: solid;
+`;
--- frontend/src/Presentational/Page/MyPage/MyProfile.tsx
+++ frontend/src/Presentational/Page/MyPage/MyProfile.tsx
@@ -2,24 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as MyStyle from '../../Components/MyComponents/MyStyle';
 import MyGlobal from '../../Components/MyComponents/MyGlobalStyle';
+import * as MyProfileBox from '../../Components/MyComponents/MyProfileComponent/MyProfileBox';
 // import MyHeader from '../../Layout/MyLayout/MyHeader';
 
 function MyProfile() {
   return (
     <MyStyle.Container>
       <MyGlobal />
-      <div className="MyProfile">
-        <h1>MyProfile</h1>
-        <MyStyle.MyButton>
-          <Link to="/mypage/dict">나의 도감</Link>
-        </MyStyle.MyButton>
-        <MyStyle.MyButton>
-          <Link to="/mypage/posts">내가 쓴 글</Link>
-        </MyStyle.MyButton>
-        <MyStyle.MyButton>
-          <Link to="/mypage/reward">나의 업적</Link>
-        </MyStyle.MyButton>
-      </div>
+      <MyProfileBox.ProfileMainContainer>
+        <MyProfileBox.ProfileSubContainer>
+          <MyProfileBox.Photo>
+            <h1>hi</h1>
+          </MyProfileBox.Photo>
+          <MyProfileBox.Nick>
+            <h1>hello</h1>
+          </MyProfileBox.Nick>
+        </MyProfileBox.ProfileSubContainer>
+        <MyProfileBox.ProfileSubContainer>
+          <MyProfileBox.ProfileButtonContainer>
+            <Link to="/mypage/dict">나의 도감</Link>
+          </MyProfileBox.ProfileButtonContainer>
+          <MyProfileBox.ProfileButtonContainer>
+            <Link to="/mypage/posts">내가 쓴 글 </Link>
+          </MyProfileBox.ProfileButtonContainer>
+          <MyProfileBox.ProfileButtonContainer>
+            <Link to="/mypage/reward">나의 업적</Link>
+          </MyProfileBox.ProfileButtonContainer>
+        </MyProfileBox.ProfileSubContainer>
+      </MyProfileBox.ProfileMainContainer>
     </MyStyle.Container>
   );
 }
