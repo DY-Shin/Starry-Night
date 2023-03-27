32c88ed1212ee0cce19a4f253577afb1f6962daa
Lee_Jungyun <leejy96kr@naver.com>
2023-03-27 17:37:53 +0900

    fix: MyProfile format Done

--- frontend/src/Presentational/Components/MyComponents/MyProfileComponent/MyProfileBox.tsx
+++ frontend/src/Presentational/Components/MyComponents/MyProfileComponent/MyProfileBox.tsx
@@ -4,32 +4,34 @@ export const ProfileMainContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  background-color: black;
-  height: 90%;
+  /* background-color: black; */
+  height: 90vh;
   width: 100%;
 `;
 
 export const ProfileSubContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  background-color: white;
+  /* background-color: white; */
   height: 90%;
   width: 50%;
 `;
 
 export const ProfileButtonContainer = styled.div`
+  color: gold;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  background-color: red;
-  height: 90%;
-  width: 50%;
+  /* background-color: red; */
+  height: 30%;
+  width: 90%;
 `;
 
 export const Photo = styled.div`
-  width: 200px;
-  height: 200px;
+  width: 300px;
+  height: 300px;
   display: flex;
   font-size: 1.5rem;
   color: whitesmoke;
@@ -43,16 +45,26 @@ export const Photo = styled.div`
 `;
 
 export const Nick = styled.div`
-  width: 200px;
-  height: 200px;
+  width: 300px;
+  height: 100px;
   display: flex;
   font-size: 1.5rem;
   color: whitesmoke;
   align-items: center;
   justify-content: center;
   background: #016040;
-  border-radius: 10%;
+  border-radius: 10px;
   border-color: purple;
   border-width: 3px;
   border-style: solid;
+`;
+
+export const MyProfileContent = styled.div`
+  display: flex;
+  color: gold;
+  align-items: center;
+  justify-content: left;
+  font-size: x-large;
+  height: 50%;
+  width: 200px;
 `;
--- frontend/src/Presentational/Page/MyPage/MyProfile.tsx
+++ frontend/src/Presentational/Page/MyPage/MyProfile.tsx
@@ -20,13 +20,28 @@ function MyProfile() {
         </MyProfileBox.ProfileSubContainer>
         <MyProfileBox.ProfileSubContainer>
           <MyProfileBox.ProfileButtonContainer>
-            <Link to="/mypage/dict">나의 도감</Link>
+            <MyStyle.MyButton>
+              <Link to="/mypage/dict">나의 도감</Link>
+            </MyStyle.MyButton>
+            <MyProfileBox.MyProfileContent>
+              <h1>23/60</h1>
+            </MyProfileBox.MyProfileContent>
           </MyProfileBox.ProfileButtonContainer>
           <MyProfileBox.ProfileButtonContainer>
-            <Link to="/mypage/posts">내가 쓴 글 </Link>
+            <MyStyle.MyButton>
+              <Link to="/mypage/posts">내가 쓴 글 </Link>
+            </MyStyle.MyButton>
+            <MyProfileBox.MyProfileContent>
+              <h1>123개</h1>
+            </MyProfileBox.MyProfileContent>
           </MyProfileBox.ProfileButtonContainer>
           <MyProfileBox.ProfileButtonContainer>
-            <Link to="/mypage/reward">나의 업적</Link>
+            <MyStyle.MyButton>
+              <Link to="/mypage/reward">나의 업적</Link>
+            </MyStyle.MyButton>
+            <MyProfileBox.MyProfileContent>
+              <h1>2/8</h1>
+            </MyProfileBox.MyProfileContent>
           </MyProfileBox.ProfileButtonContainer>
         </MyProfileBox.ProfileSubContainer>
       </MyProfileBox.ProfileMainContainer>
