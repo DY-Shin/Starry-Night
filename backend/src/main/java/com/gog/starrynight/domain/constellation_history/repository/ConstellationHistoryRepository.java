adced9b69561375d55297da9584e35c01ca7038e
이희수 <2heedol@naver.com>
2023-03-29 10:42:15 +0900

    feat: 게시물 수정 기능 구현

--- backend/src/main/java/com/gog/starrynight/domain/constellation_history/repository/ConstellationHistoryRepository.java
+++ backend/src/main/java/com/gog/starrynight/domain/constellation_history/repository/ConstellationHistoryRepository.java
@@ -3,5 +3,8 @@ package com.gog.starrynight.domain.constellation_history.repository;
 import com.gog.starrynight.domain.constellation_history.entity.ConstellationHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ConstellationHistoryRepository extends JpaRepository<ConstellationHistory, Long> {
+    Optional<ConstellationHistory> findByConstellationIdAndPostId(Long constellationId, Long postId);
 }
