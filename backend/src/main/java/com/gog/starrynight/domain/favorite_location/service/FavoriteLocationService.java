6654554ac3cb8a1742d9c33925ce777d34e464a6
이희수 <2heedol@naver.com>
2023-03-29 15:22:26 +0900

    refactor: PagingRequest 추가로 인한 리팩토링

--- backend/src/main/java/com/gog/starrynight/domain/favorite_location/service/FavoriteLocationService.java
+++ backend/src/main/java/com/gog/starrynight/domain/favorite_location/service/FavoriteLocationService.java
@@ -1,14 +1,13 @@
 package com.gog.starrynight.domain.favorite_location.service;
 
 import com.gog.starrynight.common.dto.PagedResult;
+import com.gog.starrynight.common.dto.PagingRequest;
 import com.gog.starrynight.common.exception.ResourceAlreadyExistsException;
 import com.gog.starrynight.common.exception.ResourceNotFoundException;
 import com.gog.starrynight.domain.favorite_location.dto.FavoriteLocationCreateRequest;
 import com.gog.starrynight.domain.favorite_location.dto.FavoriteLocationInfo;
-import com.gog.starrynight.domain.favorite_location.dto.FavoriteLocationGetRequest;
 import com.gog.starrynight.domain.favorite_location.entity.FavoriteLocation;
 import com.gog.starrynight.domain.favorite_location.repository.FavoriteLocationRepository;
-import com.gog.starrynight.domain.user.dto.UserSimpleInfo;
 import com.gog.starrynight.domain.user.entity.User;
 import com.gog.starrynight.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -60,7 +59,7 @@ public class FavoriteLocationService {
         favoriteLocationRepository.delete(favoriteLocation);
     }
 
-    public PagedResult<FavoriteLocationInfo> getFavoriteLocation(Long requesterId, FavoriteLocationGetRequest dto) {
+    public PagedResult<FavoriteLocationInfo> getFavoriteLocation(Long requesterId, PagingRequest dto) {
         User requester = userRepository.findById(requesterId)
                 .orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 회원입니다."));
 
--- backend/src/main/java/com/gog/starrynight/domain/post_like/service/PostLikeService.java
+++ backend/src/main/java/com/gog/starrynight/domain/post_like/service/PostLikeService.java
@@ -1,11 +1,11 @@
 package com.gog.starrynight.domain.post_like.service;
 
 import com.gog.starrynight.common.dto.PagedResult;
+import com.gog.starrynight.common.dto.PagingRequest;
 import com.gog.starrynight.common.exception.ResourceAlreadyExistsException;
 import com.gog.starrynight.common.exception.ResourceNotFoundException;
 import com.gog.starrynight.domain.post.entity.Post;
 import com.gog.starrynight.domain.post.repository.PostRepository;
-import com.gog.starrynight.domain.post_like.dto.GetPostLikersRequest;
 import com.gog.starrynight.domain.post_like.entity.PostLike;
 import com.gog.starrynight.domain.post_like.repository.PostLikeRepository;
 import com.gog.starrynight.domain.user.dto.UserSimpleInfo;
@@ -66,7 +66,7 @@ public class PostLikeService {
         postLikeRepository.delete(postLike);
     }
 
-    public PagedResult<UserSimpleInfo> getPostLikers(Long postId, GetPostLikersRequest dto) {
+    public PagedResult<UserSimpleInfo> getPostLikers(Long postId, PagingRequest dto) {
         Sort.Direction direction = dto.getDirection().equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, dto.getSort());
         Pageable pageable = PageRequest.of(dto.getPage(), dto.getSize(), sort);
