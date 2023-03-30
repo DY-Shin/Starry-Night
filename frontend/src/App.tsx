ba995b61bf6742c05d8c8069ae297e5f6ddd3859
Lee_Jungyun <leejy96kr@naver.com>
2023-03-30 13:42:11 +0900

    fix: apptsx indextsx changed into origin ver

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,10 @@
-// 원본
-// import React from 'react';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       StarryNight
-//       <div>FrontEnd</div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-// import Header from './Presentational/Common/Layout/Header/Header';
-import Footer from './Presentational/Common/Layout/Footer/Footer';
-import MyHeader from './Presentational/Layout/MyLayout/MyHeader';
-
-// import MyProfile from './Presentational/Layout/MyProfile';
 
 function App() {
   return (
     <div className="App">
-      <MyHeader />
-      <Footer />
+      StarryNight
+      <div>FrontEnd</div>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,31 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-import MyProfile from './Presentational/Page/MyPage/MyProfile';
-import MyDict from './Presentational/Page/MyPage/MyDict';
-import MyPosts from './Presentational/Page/MyPage/MyPost';
-import MyReward from './Presentational/Page/MyPage/MyReward';
-import Header from './Presentational/Common/Layout/Header/Header';
-import Footer from './Presentational/Common/Layout/Footer/Footer';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/mypage" element={<MyProfile />} />
-      <Route path="/mypage/dict" element={<MyDict />} />
-      <Route path="/mypage/posts" element={<MyPosts />} />
-      <Route path="/mypage/reward" element={<MyReward />} />
-    </Routes>
-    <Footer />
-  </Router>,
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
