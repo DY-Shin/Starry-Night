f65d1e8a7e5f466e9410205ab327b48cec5f6a58
Lee_Jungyun <leejy96kr@naver.com>
2023-03-31 11:06:21 +0900

    fix: MyDictLayout with Sol

--- frontend/src/Presentational/Layout/MyLayout/MyDict/MyDictDongBot.tsx
+++ frontend/src/Presentational/Layout/MyLayout/MyDict/MyDictDongBot.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as Dictainer from '../../../Components/MyComponents/MyDictComponent/MyDictContainer';
 
-function MyDictBei() {
+function MyDictDongBot() {
   return (
     <Dictainer.DictSubBox>
       <Dictainer.BjrBox>
@@ -44,4 +44,4 @@ function MyDictBei() {
   );
 }
 
-export default MyDictBei;
+export default MyDictDongBot;
