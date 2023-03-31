f909ceea30c4684dfffac1676f5bd96ebf7b8df4
kavin816@naver.com <kavin816@naver.com>
2023-03-31 11:12:18 +0900

    feat: 도전과제 별자리 등록 구현

--- backend/src/main/java/com/gog/starrynight/domain/achievement_constellation/service/AchievementConstellationService.java
+++ backend/src/main/java/com/gog/starrynight/domain/achievement_constellation/service/AchievementConstellationService.java
@@ -1,6 +1,17 @@
 package com.gog.starrynight.domain.achievement_constellation.service;
 
+import com.gog.starrynight.common.exception.ResourceAlreadyExistsException;
+import com.gog.starrynight.common.exception.ResourceNotFoundException;
+import com.gog.starrynight.domain.achievement.dto.AchievementInfo;
+import com.gog.starrynight.domain.achievement.entity.Achievement;
+import com.gog.starrynight.domain.achievement.repository.AchievementRepository;
+import com.gog.starrynight.domain.achievement_constellation.dto.AchievementConstellationCreateRequest;
+import com.gog.starrynight.domain.achievement_constellation.dto.AchievementConstellationInfo;
+import com.gog.starrynight.domain.achievement_constellation.entity.AchievementConstellation;
 import com.gog.starrynight.domain.achievement_constellation.repository.AchievementConstellationRepository;
+import com.gog.starrynight.domain.constellation.dto.ConstellationSimpleInfo;
+import com.gog.starrynight.domain.constellation.entity.Constellation;
+import com.gog.starrynight.domain.constellation.repository.ConstellationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +21,31 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AchievementConstellationService {
     private final AchievementConstellationRepository achievementConstellationRepository;
+    private final AchievementRepository achievementRepository;
+    private final ConstellationRepository constellationRepository;
+
+    @Transactional
+    public AchievementConstellationInfo createAchievementConstellation(AchievementConstellationCreateRequest dto) {
+        Achievement achievement = achievementRepository.findById(dto.getAchievementId())
+                .orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 도전과제입니다."));
+
+        Constellation constellation = constellationRepository.findById(dto.getConstellationId())
+                .orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 별자리입니다."));
+
+        if (achievementConstellationRepository.findByAchievementAndConstellation(achievement, constellation).isPresent()){
+            throw new ResourceAlreadyExistsException("해당 도전과제에 이미 등록된 별자리입니다.");
+        }
+
+        AchievementConstellation achievementConstellation = AchievementConstellation.builder()
+                .achievement(achievement)
+                .constellation(constellation)
+                .build();
+
+        achievementConstellationRepository.save(achievementConstellation);
+
+        AchievementInfo achievementInfo = new AchievementInfo(achievement);
+        ConstellationSimpleInfo constellationSimpleInfo = new ConstellationSimpleInfo(constellation);
+
+        return new AchievementConstellationInfo(achievementInfo, constellationSimpleInfo);
+    }
 }
