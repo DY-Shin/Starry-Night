7a873a01bc8584ae3bfc83a91e8f89ff7d77dd66
이희수 <2heedol@naver.com>
2023-04-01 23:48:01 +0900

    fix: UserAchievementRepository 수정

--- backend/src/main/java/com/gog/starrynight/domain/user_achievement/repository/UserAchievementRepository.java
+++ backend/src/main/java/com/gog/starrynight/domain/user_achievement/repository/UserAchievementRepository.java
@@ -1,8 +1,9 @@
 package com.gog.starrynight.domain.user_achievement.repository;
 
-import com.gog.starrynight.domain.achievement.entity.Achievement;
+import com.gog.starrynight.domain.user_achievement.entity.UserAchievement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAchievementRepository extends JpaRepository<Achievement, Long> {
+public interface UserAchievementRepository extends JpaRepository<UserAchievement, Long> {
+
 
 }
