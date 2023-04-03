a97b7282e75bbed13760456bf3ba2adc441f3df6
leejinsung <jsmon3536@gmail.com>
2023-04-03 14:18:42 +0900

    Feat: 업적 api 연결

--- frontend/src/Presentational/Page/MyPage/MyReward.tsx
+++ frontend/src/Presentational/Page/MyPage/MyReward.tsx
@@ -1,29 +1,50 @@
-import React from 'react';
+/* eslint-disable no-undef */
+/* eslint-disable array-callback-return */
+import React, { useEffect, useState } from 'react';
 import * as MyStyle from '../../Components/MyComponents/MyStyle';
 import MyGlobal from '../../Components/MyComponents/MyGlobalStyle';
 import MyHeader from '../../Layout/MyLayout/MyHeader';
 import * as MyBadge from '../../Components/MyBadge';
+import api from '../../Components/api';
+// import { Name } from './../../Components/MainPage/MainContel_Style';
 
 function MyReward() {
+  const [rewards, setRewards] = useState([]);
+
+  const getReward = async () => {
+    await api.get(`/achievements`, { withCredentials: true }).then((res) => {
+      setRewards(res.data.data);
+      console.log(res.data.data);
+    });
+  };
+
+  // const makeReward = async () => {
+  //   await api.post(`/achievements` ,{ withCredentials: true }).then((res) => {
+  //     setRewards(res.data.data);
+  //     console.log(res.data.data);
+  //   });
+  // };
+
+  useEffect(() => {
+    getReward();
+  }, []);
+
+  const renderRewards = (): JSX.Element[] => {
+    const filteredRewards = rewards.map((reward: any) => {
+      if (reward.completed === false) {
+        return <MyBadge.Badge key={reward.id}>{reward.name}</MyBadge.Badge>;
+      }
+      return <MyBadge.Badge>123</MyBadge.Badge>;
+    });
+    return filteredRewards;
+  };
+
   return (
     <MyStyle.Container>
       <MyGlobal />
       <MyHeader />
-      <MyBadge.RewardContainer>
-        <MyBadge.Badge>
-          곰 두마리가 <br /> 북쪽에 있어
-        </MyBadge.Badge>
-        <MyBadge.Badge>
-          낮에는 꽃잎 <br /> 밤에는 별빛
-        </MyBadge.Badge>
-        <MyBadge.Badge>
-          은하에 흐르는 <br /> 한여름밤의 꿈
-        </MyBadge.Badge>
-        <MyBadge.Badge>
-          낙엽에 적혀진 <br /> 영웅의 이야기
-        </MyBadge.Badge>
-      </MyBadge.RewardContainer>
-      <MyBadge.RewardContainer>
+      <MyBadge.RewardContainer>{renderRewards()}</MyBadge.RewardContainer>
+      {/* <MyBadge.RewardContainer>
         <MyBadge.Badge>
           땅에선 캐롤이 <br /> 하늘에선 별이
         </MyBadge.Badge>
@@ -36,7 +57,7 @@ function MyReward() {
         <MyBadge.Badge>
           눈으로 볼 수 <br /> 있는건 다 봤어
         </MyBadge.Badge>
-      </MyBadge.RewardContainer>
+      </MyBadge.RewardContainer> */}
     </MyStyle.Container>
   );
 }
