a485f04eb2ac23d2362b3bd6b809ceaea355964c
leejinsung <jsmon3536@gmail.com>
2023-04-04 10:39:30 +0900

    Feat: 상단 메뉴바 추가 / 폰트 추가

--- frontend/src/Presentational/Page/MyPage/MyReward.tsx
+++ frontend/src/Presentational/Page/MyPage/MyReward.tsx
@@ -1,63 +1,72 @@
+/* eslint-disable react/jsx-pascal-case */
 /* eslint-disable no-undef */
+/* eslint-disable operator-assignment */
 /* eslint-disable array-callback-return */
+/* eslint-disable no-plusplus */
 import React, { useEffect, useState } from 'react';
 import * as MyStyle from '../../Components/MyComponents/MyStyle';
 import MyGlobal from '../../Components/MyComponents/MyGlobalStyle';
 import MyHeader from '../../Layout/MyLayout/MyHeader';
 import * as MyBadge from '../../Components/MyBadge';
 import api from '../../Components/api';
-// import { Name } from './../../Components/MainPage/MainContel_Style';
+import Header from '../../Components/MainPage/Header';
 
 function MyReward() {
   const [rewards, setRewards] = useState([]);
+  const [stars, setStars] = useState<{ [key: string]: [number, number] }>({});
 
+  // 도전과제 목록 가져오기
   const getReward = async () => {
     await api.get(`/achievements`, { withCredentials: true }).then((res) => {
       setRewards(res.data.data);
-      console.log(res.data.data);
     });
   };
 
-  // const makeReward = async () => {
-  //   await api.post(`/achievements` ,{ withCredentials: true }).then((res) => {
-  //     setRewards(res.data.data);
-  //     console.log(res.data.data);
-  //   });
-  // };
+  // 도전과제별 별자리 가져오기
+  const getOneStars = async (id: string) => {
+    await api.get(`/achievements/${id}/constellations`, { withCredentials: true }).then((res) => {
+      const temps = res.data.data;
+      const count = temps.filter((star: { completed: any }) => star.completed).length;
+      const total = temps.length;
+      setStars((prevStars) => ({ ...prevStars, [id]: [total, count] }));
+    });
+  };
 
   useEffect(() => {
     getReward();
+    for (let i = 1; i < 6; i++) {
+      getOneStars(`${i}`);
+    }
   }, []);
 
   const renderRewards = (): JSX.Element[] => {
     const filteredRewards = rewards.map((reward: any) => {
-      if (reward.completed === false) {
-        return <MyBadge.Badge key={reward.id}>{reward.name}</MyBadge.Badge>;
+      const [total, count] = stars[reward.id] || [0, 0];
+      if (!reward.completed) {
+        return (
+          <MyBadge.NotCompletedBadge key={reward.id}>
+            {reward.name}
+            <MyBadge.modal>{`${count}/${total}`}</MyBadge.modal>
+          </MyBadge.NotCompletedBadge>
+        );
       }
-      return <MyBadge.Badge>123</MyBadge.Badge>;
+      return (
+        <MyBadge.CompletedBadge key={reward.id}>
+          {reward.name}
+          <MyBadge.modal>{`${count}/${total}`}</MyBadge.modal>
+        </MyBadge.CompletedBadge>
+      );
     });
+
     return filteredRewards;
   };
 
   return (
     <MyStyle.Container>
       <MyGlobal />
+      {/* <Header /> */}
       <MyHeader />
       <MyBadge.RewardContainer>{renderRewards()}</MyBadge.RewardContainer>
-      {/* <MyBadge.RewardContainer>
-        <MyBadge.Badge>
-          땅에선 캐롤이 <br /> 하늘에선 별이
-        </MyBadge.Badge>
-        <MyBadge.Badge>
-          멀지만 가까운 <br /> 태양계 가족
-        </MyBadge.Badge>
-        <MyBadge.Badge>
-          천개의 별도 <br /> 첫 관측부터
-        </MyBadge.Badge>
-        <MyBadge.Badge>
-          눈으로 볼 수 <br /> 있는건 다 봤어
-        </MyBadge.Badge>
-      </MyBadge.RewardContainer> */}
     </MyStyle.Container>
   );
 }
