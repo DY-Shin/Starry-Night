ed418d85a8c137e2f7d047aaa16a293b1e24a193
Lee_Jungyun <leejy96kr@naver.com>
2023-04-04 15:28:54 +0900

    fix: myPostbox slider info on

--- frontend/src/Presentational/Components/MyComponents/MyPostComponents/MyPostBoxStyle.tsx
+++ frontend/src/Presentational/Components/MyComponents/MyPostComponents/MyPostBoxStyle.tsx
@@ -1,14 +1,22 @@
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
+export const SliderWrapper = styled.div`
+  width: 100%;
+  height: 90%;
+  display: flex;
+`;
+
 export const Slider = styled.div`
   position: relative;
-  background-color: aliceblue;
   margin-right: 4.5em;
   margin-left: 4.5em;
+  width: 100%;
+  height: 100%;
 `;
 
 export const Row = styled(motion.div)`
+  margin-top: 10%;
   display: grid;
   gap: 10px;
   position: absolute;
@@ -18,13 +26,29 @@ export const Row = styled(motion.div)`
 
 export const rowVariants = {
   hidden: {
-    x: window.outerWidth + 10,
+    x: window.outerWidth + 30,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.outerWidth - 10,
+    x: -window.outerWidth - 30,
+  },
+};
+
+export const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    zIndex: 99,
+    scale: 1.2,
+    y: -50,
+    transition: {
+      delay: 0.3,
+      duration: 0.3,
+      type: 'tween',
+    },
   },
 };
 
@@ -33,4 +57,25 @@ export const Box = styled(motion.div)`
   color: white;
   font-size: 50px;
   height: 200px;
+  position: relative;
 `;
+
+export const PostInfo = styled(motion.div)`
+  padding: 20px;
+  background-color: grey;
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+`;
+
+export const PostInfoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.3,
+      duaration: 0.1,
+      type: 'tween',
+    },
+  },
+};
--- frontend/src/Presentational/Layout/MyLayout/MyPost/MyPostBox.tsx
+++ frontend/src/Presentational/Layout/MyLayout/MyPost/MyPostBox.tsx
@@ -2,33 +2,45 @@ import React, { useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import * as MyPostBox from '../../../Components/MyComponents/MyPostComponents/MyPostBoxStyle';
 
+const offset = 10;
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
 function MyArticle() {
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
   const toggleLeaving = () => setLeaving((prev) => !prev);
   const increaseIndex = () => {
-    if (leaving) return;
-    toggleLeaving();
-    setIndex((prev) => prev + 1);
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalPosts = data.length;
+      const maxIndex = Math.ceil(totalPosts / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
   };
 
   return (
-    <MyPostBox.Slider>
-      <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
-        <MyPostBox.Row
-          variants={MyPostBox.rowVariants}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          transition={{ type: 'tween', duration: 1 }}
-          key={index}
-        >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
-            <MyPostBox.Box key={i}>{i}</MyPostBox.Box>
-          ))}
-        </MyPostBox.Row>
-      </AnimatePresence>
-    </MyPostBox.Slider>
+    <MyPostBox.SliderWrapper onClick={increaseIndex}>
+      <MyPostBox.Slider>
+        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+          <MyPostBox.Row
+            variants={MyPostBox.rowVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ type: 'tween', duration: 1 }}
+            key={index}
+          >
+            {data.slice(offset * index, offset * index + offset).map((i) => (
+              <MyPostBox.Box key={i} variants={MyPostBox.boxVariants} whileHover="hover" initial="normal">
+                {i}
+                <MyPostBox.PostInfo variants={MyPostBox.PostInfoVariants} />
+              </MyPostBox.Box>
+            ))}
+          </MyPostBox.Row>
+        </AnimatePresence>
+      </MyPostBox.Slider>
+    </MyPostBox.SliderWrapper>
   );
 }
 
