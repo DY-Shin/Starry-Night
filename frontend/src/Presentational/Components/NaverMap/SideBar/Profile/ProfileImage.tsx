e41e566b2e6f5295aa6112832d045976f000783f
김지훈 <ji_hun23@naver.com>
2023-04-04 21:29:39 +0900

    feat: 프로필 이미지, 이름, 홈화면으로

--- frontend/src/Presentational/Components/NaverMap/SideBar/Profile/ProfileImage.tsx
+++ frontend/src/Presentational/Components/NaverMap/SideBar/Profile/ProfileImage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import * as ProfileImageStyle from './ProfileImage_Style';
+import { UserStore } from '../../../../../store';
 
 function ProfileImage() {
+  const { profileImageUrl } = UserStore();
   return (
     <ProfileImageStyle.ProfileImageWrapper>
       <ProfileImageStyle.ProfileImageBorder>
-        <ProfileImageStyle.ProfileImage />
+        {profileImageUrl !== 'null' ? (
+          <ProfileImageStyle.ProfileImage url={profileImageUrl} />
+        ) : (
+          <ProfileImageStyle.ProfileImage url="https://s3-us-west-2.amazonaws.com/s.cdpn.io/53474/atom_profile_01.jpg" />
+        )}
       </ProfileImageStyle.ProfileImageBorder>
     </ProfileImageStyle.ProfileImageWrapper>
   );
--- frontend/src/Presentational/Layout/NaverMap/SideBar/SideBarProfileArea.tsx
+++ frontend/src/Presentational/Layout/NaverMap/SideBar/SideBarProfileArea.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import { AiOutlineHome } from 'react-icons/ai';
 import * as ProfileStyle from './SideBarProFileArea_Style';
 import ProfileImage from '../../../Components/NaverMap/SideBar/Profile/ProfileImage';
+import { UserStore } from '../../../../store';
 
 function SideBarProfileArea() {
+  const { name } = UserStore();
   return (
     <ProfileStyle.ProfileWrapper>
-      <ProfileStyle.TopWrapper />
+      <ProfileStyle.TopWrapper>
+        {/* <ProfileStyle.TopText>Home</ProfileStyle.TopText> */}
+        <ProfileStyle.TopHomeLink to="/">
+          <AiOutlineHome size={25} className="icon" />
+        </ProfileStyle.TopHomeLink>
+      </ProfileStyle.TopWrapper>
       <ProfileStyle.MiddleWrapper>
         <ProfileImage />
       </ProfileStyle.MiddleWrapper>
       <ProfileStyle.BottomWrapper>
-        <ProfileStyle.BottomNameText>김지훈님 환영합니다.</ProfileStyle.BottomNameText>
+        {name !== 'null' ? (
+          <ProfileStyle.BottomNameText>{name}님 환영합니다.</ProfileStyle.BottomNameText>
+        ) : (
+          <ProfileStyle.BottomNameText>로그인이 필요합니다.</ProfileStyle.BottomNameText>
+        )}
       </ProfileStyle.BottomWrapper>
     </ProfileStyle.ProfileWrapper>
   );
