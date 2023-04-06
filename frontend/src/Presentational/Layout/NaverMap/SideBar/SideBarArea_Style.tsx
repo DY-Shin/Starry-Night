5c9f88afbb9f6fdd61b6c8869eef484484706fe6
김지훈 <ji_hun23@naver.com>
2023-04-06 14:42:48 +0900

    style: SideBarArea_Style 주석 제거, 코드 정리

--- frontend/src/Presentational/Layout/NaverMap/SideBar/SideBarArea_Style.tsx
+++ frontend/src/Presentational/Layout/NaverMap/SideBar/SideBarArea_Style.tsx
@@ -1,82 +1,70 @@
 import styled from 'styled-components';
 
-export const SidBarBody = styled.div`
-  /* background-color: white; */
-  background-color: #222430;
-  width: 350px;
-  height: 100%;
-  position: absolute;
-  /* box-shadow: 0 0 0 1px #ffffff39 inset; */
-  /* box-shadow: 0 1px 0 0 #ffffff inset; */
-  top: 0;
-  &.close {
-    left: 0;
-    transition: 500ms;
-  }
-  &.open {
-    left: -350px;
-    transition: 500ms;
-  }
-  z-index: 1;
-  & * {
-    font-family: 'PyeongChang-Bold', sans-serif; /* 새로운 코드 */
-  }
-  /* & .contentsArea {
-    overflow: auto;
-    ::-webkit-scrollbar {
-      display: none;
-    }
-  } */
-`;
-
-export const SidBarContentsArea = styled.div`
-  width: 100% - 20px;
-  /* height: calc(100% - 387px); */
-  height: calc(60% - 20px);
-  /* padding: 5px 10px 5px 10px; */
-  margin: 0 10px 0 10px;
-  overflow: auto;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-export const SidBarLine = styled.hr`
-  margin: 0 0 5px 0;
-  background-color: #646675;
-  border: 0;
-  height: 1px;
-`;
-
 export const FoldBtn = styled.div`
-  position: absolute;
-  /* background-color: white; */
-  background-color: #222430;
   width: 30px;
   height: 60px;
+  position: absolute;
   top: 45%;
+  background-color: #222430;
+  box-shadow: 0 0 0 1px #58585839 inset;
+  z-index: 1;
+
+  :hover {
+    cursor: pointer;
+  }
+
   &.close {
     left: 350px;
     transition: 500ms;
   }
+
   &.open {
     left: 0px;
     transition: 500ms;
   }
-  box-shadow: 0 0 0 1px #58585839 inset;
-  :hover {
-    cursor: pointer;
-  }
-  z-index: 1;
 `;
 
 export const FoldText = styled.p`
-  margin: 0;
   width: 100%;
   height: 100%;
+  margin: 0;
+  color: #fad9f2;
   text-align: center;
   font-size: 20px;
   font-weight: 700;
   line-height: 55px;
-  color: #fad9f2;
+`;
+
+export const SidBarBody = styled.div`
+  width: 350px;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  background-color: #222430;
+  z-index: 1;
+
+  & * {
+    font-family: 'PyeongChang-Bold', sans-serif;
+  }
+
+  &.close {
+    left: 0;
+    transition: 500ms;
+  }
+
+  &.open {
+    left: -350px;
+    transition: 500ms;
+  }
+`;
+
+export const SidBarContentsArea = styled.div`
+  width: 100% - 20px;
+  height: calc(60% - 20px);
+  margin: 0 10px 0 10px;
+  overflow: auto;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
