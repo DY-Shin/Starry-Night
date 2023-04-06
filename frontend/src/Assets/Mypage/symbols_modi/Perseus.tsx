762bc8b1e9fce40411e9bc233ee1a817c6540ecf
Lee_Jungyun <leejy96kr@naver.com>
2023-04-06 15:52:39 +0900

    feat: color change in myDict

---
+++ frontend/src/Assets/Mypage/symbols_modi/Perseus.tsx
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import { motion } from 'framer-motion';
+
+const icon = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: 'rgba(255, 255, 255, 0)',
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: 'rgba(255, 255, 255, 0)',
+  },
+};
+
+function Perseus() {
+  return (
+    <div className="container">
+      <motion.svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" viewBox="0 0 700 700">
+        <defs>
+          <clipPath id="z">
+            <path d="M70 0h559.99v559.99H70z" />
+          </clipPath>
+        </defs>
+        <g fill="none" stroke="#0492b1" strokeMiterlimit="10" strokeWidth="41.791" clipPath="url(#z)">
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m234.49 653.26h335"
+          />
+
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m401.99 485.76v167.5"
+          />
+
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m234.49 318.26c-92.503 0-167.5 74.999-167.5 167.5"
+          />
+
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m401.99 318.26c-92.503 0-167.5 74.999-167.5 167.5 0 92.497-74.999 167.5-167.5 167.5"
+          />
+
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m569.49 318.26c92.503 0 167.5 74.999 167.5 167.5"
+          />
+
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m401.99 318.26c92.497 0 167.5 74.999 167.5 167.5 0 92.497 74.999 167.5 167.5 167.5"
+          />
+
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m569.49 318.26c0 92.503-74.999 167.5-167.5 167.5-92.503 0-167.5-74.999-167.5-167.5 0-92.497 74.999-167.5 167.5-167.5 92.497 0 167.5 75.005 167.5 167.5z"
+          />
+        </g>
+      </motion.svg>
+    </div>
+  );
+}
+
+export default Perseus;
---
+++ frontend/src/Assets/Mypage/symbols_modi/Vulpecula.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { motion } from 'framer-motion';
+
+const icon = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: 'rgba(255, 255, 255, 0)',
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: 'rgba(255, 255, 255, 0)',
+  },
+};
+
+function Vulpecula() {
+  return (
+    <div className="container">
+      <motion.svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" viewBox="0 0 700 700">
+        <defs>
+          <clipPath id="z">
+            <path d="M70 0h559.99v559.99H70z" />
+          </clipPath>
+        </defs>
+        <g fill="none" stroke="#0492b1" strokeMiterlimit="10" strokeWidth="41.791" clipPath="url(#z)">
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m221.41 401.22h361.17"
+          />
+
+          <motion.path
+            fill="#0492b1"
+            variants={icon}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'easeInOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+            }}
+            transform="matrix(.69652 0 0 -.69652 70 560)"
+            d="m66.991 692.13 335-580.24 335 580.24"
+          />
+        </g>
+      </motion.svg>
+    </div>
+  );
+}
+
+export default Vulpecula;
