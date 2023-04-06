f47cde71893901f8707db38c0136cb4b0efc1ccf
김지훈 <ji_hun23@naver.com>
2023-04-06 15:16:49 +0900

    style: SideBarProFileArea_Style 주석 제거, 코드 정리

--- frontend/src/Presentational/Layout/NaverMap/SideBar/SideBarProFileArea_Style.tsx
+++ frontend/src/Presentational/Layout/NaverMap/SideBar/SideBarProFileArea_Style.tsx
@@ -3,16 +3,10 @@ import { Link } from 'react-router-dom';
 
 export const ProfileWrapper = styled.div`
   width: 100%;
-  /* height: 190px; */
   height: 20%;
   background-color: white;
   background-image: url('https://images.unsplash.com/photo-1528722828814-77b9b83aafb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
   background-size: cover;
-`;
-
-export const TopHomeLink = styled(Link)`
-  margin: 0;
-  color: white;
 `;
 
 export const TopWrapper = styled.div`
@@ -21,6 +15,7 @@ export const TopWrapper = styled.div`
   background-color: #00000062;
   display: flex;
   justify-content: end;
+
   & .icon {
     margin: 2px 10px 0 0;
     cursor: pointer;
@@ -28,27 +23,31 @@ export const TopWrapper = styled.div`
   }
 `;
 
+export const TopHomeLink = styled(Link)`
+  margin: 0;
+  color: white;
+`;
+
 export const MiddleWrapper = styled.div`
   width: 100%;
   height: 50%;
-  /* background-color: green; */
 `;
 
 export const BottomWrapper = styled.div`
   width: 100%;
   height: 50px;
-  /* background-color: #00000062; */
 `;
 
 export const BottomNameText = styled.p`
   margin: 0;
+  margin-left: 30px;
   color: white;
   font-size: large;
   font-weight: bold;
   text-align: left;
-  margin-left: 30px;
   text-shadow: 2px 2px 2px gray;
   line-height: 60px;
+
   :hover {
     cursor: default;
   }
